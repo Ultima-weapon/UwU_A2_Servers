9cdaad7cb89d3532f56bf2a3d987d624136351e5
Ultima-weapon <andrewsford@cox.net>
2017-05-06 22:21:17 -0700

    Add files via upload

---
+++ MPMissions/UwU.Napf/custom/customloot/Category/NeutralShotgunsandSingleshotAmmo.hpp
@@ -0,0 +1,11 @@
+// Ammo - Shotgun & Rifle
+class Category_7017 {
+	class 15Rnd_W1866_Slug 			{	type = "trade_items";	buy[] ={60,"Coins"};	sell[] ={30,"Coins"};	};
+	class 2Rnd_shotgun_74Pellets 	{	type = "trade_items";	buy[] ={60,"Coins"};	sell[] ={30,"Coins"};	};
+	class 2Rnd_shotgun_74Slug 		{	type = "trade_items";	buy[] ={60,"Coins"};	sell[] ={30,"Coins"};	};
+	class 8Rnd_B_Beneli_74Slug 		{	type = "trade_items";	buy[] ={60,"Coins"};	sell[] ={30,"Coins"};	};
+	class 8Rnd_B_Beneli_Pellets 	{	type = "trade_items";	buy[] ={60,"Coins"};	sell[] ={30,"Coins"};	};
+	class WoodenArrow 				{	type = "trade_items";	buy[] ={60,"Coins"};	sell[] ={30,"Coins"};	};
+	class Quiver 					{	type = "trade_items";	buy[] ={60,"Coins"};	sell[] ={30,"Coins"};	};
+	class 10x_303 					{	type = "trade_items";	buy[] ={60,"Coins"};	sell[] ={30,"Coins"};	};
+};
