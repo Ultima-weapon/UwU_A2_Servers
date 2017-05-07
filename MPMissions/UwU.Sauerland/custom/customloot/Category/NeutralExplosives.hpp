f768d6afa9fff8fe3fd05deb9594ba1164f8ea50
Ultima-weapon <andrewsford@cox.net>
2017-05-06 22:27:48 -0700

    Add files via upload

---
+++ MPMissions/UwU.Sauerland/custom/customloot/Category/NeutralExplosives.hpp
@@ -0,0 +1,6 @@
+// Grenades
+class Category_7006 {
+	class HandGrenade_west 	{	type = "trade_items";	buy[] ={4000,"Coins"};	sell[] ={2000,"Coins"};	};
+	class 1Rnd_HE_M203 		{	type = "trade_items";	buy[] ={4000,"Coins"};	sell[] ={2000,"Coins"};	};
+	class HandGrenade_east 	{	type = "trade_items";	buy[] ={4000,"Coins"};	sell[] ={2000,"Coins"};	};
+};
