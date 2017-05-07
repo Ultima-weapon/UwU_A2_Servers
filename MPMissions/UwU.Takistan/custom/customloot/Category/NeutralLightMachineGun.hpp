a9fbba4cc1fb56b90939a4c43f16deea6f4915e3
Ultima-weapon <andrewsford@cox.net>
2017-05-06 22:34:59 -0700

    Add files via upload

---
+++ MPMissions/UwU.Takistan/custom/customloot/Category/NeutralLightMachineGun.hpp
@@ -0,0 +1,13 @@
+// Machine Guns
+class Category_7009 {
+	class M249		 	{	type = "trade_weapons";	buy[] ={60000,"Coins"};	sell[] ={17500,"Coins"};	};
+	class M249_DZ 		{	type = "trade_weapons";	buy[] ={60000,"Coins"};	sell[] ={17500,"Coins"};	};
+	class M249_EP1	 	{	type = "trade_weapons";	buy[] ={60000,"Coins"};	sell[] ={17500,"Coins"};	};
+	class M249_m145_EP1	{	type = "trade_weapons";	buy[] ={60000,"Coins"};	sell[] ={17500,"Coins"};	};
+	class M249_m145_EP1_DZE 	{	type = "trade_weapons";	buy[] ={60000,"Coins"};	sell[] ={17500,"Coins"};	};
+	class M249_TWS_EP1 	{	type = "trade_weapons";	buy[] ={60000,"Coins"};	sell[] ={17500,"Coins"};	};
+	class vil_M249_Para	{	type = "trade_weapons";	buy[] ={60000,"Coins"};	sell[] ={17500,"Coins"};	};
+	class M240_DZ 		{	type = "trade_weapons";	buy[] ={65000,"Coins"};	sell[] ={20000,"Coins"};	};
+	class Mk_48_DZ 		{	type = "trade_weapons";	buy[] ={65000,"Coins"};	sell[] ={20000,"Coins"};	};
+	class Pecheneg_DZ 	{	type = "trade_weapons";	buy[] ={62500,"Coins"};	sell[] ={20000,"Coins"};	};
+};
---
+++ MPMissions/UwU.Takistan/custom/customloot/Category/NeutralWholesale.hpp
@@ -0,0 +1,26 @@
+// Bulk Ammo
+class Category_7028 {
+	class bulk_15Rnd_9x19_M9SD 			{	type = "trade_items";	buy[] ={2000,"Coins"}	;sell[] ={1000,"Coins"};	};
+	class bulk_17Rnd_9x19_glock17 		{	type = "trade_items";	buy[] ={2000,"Coins"}	;sell[] ={1000,"Coins"};	};
+	class bulk_30Rnd_556x45_StanagSD 	{	type = "trade_items";	buy[] ={2000,"Coins"}	;sell[] ={1000,"Coins"};	};
+	class bulk_30Rnd_9x19_MP5SD 		{	type = "trade_items";	buy[] ={2000,"Coins"}	;sell[] ={1000,"Coins"};	};
+};
+// Bulk Building Supplies
+class Category_7029 {
+	class bulk_ItemSandbag 				{	type = "trade_items";	buy[] ={48000,"Coins"}	;sell[] ={24000,"Coins"};	};
+	class bulk_ItemTankTrap 			{	type = "trade_items";	buy[] ={3600,"Coins"}	;sell[] ={1800,"Coins"};	};
+	class bulk_ItemWire 				{	type = "trade_items";	buy[] ={2160,"Coins"}	;sell[] ={1080,"Coins"};	};
+	class bulk_PartGeneric 				{	type = "trade_items";	buy[] ={7200,"Coins"}	;sell[] ={3600,"Coins"};		};
+	class PartPlywoodPack 				{	type = "trade_items";	buy[] ={600,"Coins"}	;sell[] ={300,"Coins"};		};
+	class PartPlankPack 				{	type = "trade_items";	buy[] ={300,"Coins"}	;sell[] ={150,"Coins"};		};
+};
+// Building Supplies
+class Category_7030 {
+	class metal_floor_kit				{	type = "trade_items";	buy[] ={80000,"Coins"}	;sell[] ={20000,"Coins"};	};
+	class cinder_wall_kit				{	type = "trade_items";	buy[] ={80000,"Coins"}	;sell[] ={20000,"Coins"};	};
+	class cinder_door_kit				{	type = "trade_items";	buy[] ={80000,"Coins"}	;sell[] ={20000,"Coins"};	};
+	class cinder_garage_kit				{	type = "trade_items";	buy[] ={80000,"Coins"}	;sell[] ={20000,"Coins"};	};
+	class CinderBlocks 					{	type = "trade_items";	buy[] ={10000,"Coins"}	;sell[] ={5000,"Coins"};	};
+	class MortarBucket 					{	type = "trade_items";	buy[] ={10000,"Coins"}	;sell[] ={5000,"Coins"};	};
+	class ItemFuelBarrelEmpty 			{	type = "trade_items";	buy[] ={3000,"Coins"}	;sell[] ={1500,"Coins"};	};	
+};
